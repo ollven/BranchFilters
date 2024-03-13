7d8a848417342ae91e84d010fac936eae9d3622b
ollven <ollven@yandex.ru>
2024-03-13 17:11:49 +0100

    add test which runs 2 minutes

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void testThatAlwaysFails() throws InterruptedException {
+        Thread.sleep(120000);
+        Assertions.assertTrue(true);    }
+
+}
