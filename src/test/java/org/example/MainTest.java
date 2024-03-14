203a330e611e8c37d0caec739d7a131b50b5e883
Olga Sventukh <ollven@yandex.ru>
2024-03-14 15:52:59 +0100

    reduce duration

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class MainTest {
     @Test
     public void testThatAlwaysFails() throws InterruptedException {
-        Thread.sleep(120000);
+        Thread.sleep(100);
         Assertions.assertTrue(false);    }
 
 }
